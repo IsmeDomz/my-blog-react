e1a0f0940bf4cff2b1b0791e0bd7a95b59601e73
IsmeDomz <insokudom@gmail.com>
2026-08-04 13:30:05 +0700

    intital commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/my-blog-react/', // Set the base path to the subdirectory where your app is hosted
 })
